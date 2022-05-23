4d4d8010989c8e3b2b1b2034fa13140e1e6cf243
shova29 <halima2996@gmail.com>
2022-05-24 05:21:07 +0600

    server connect with db

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl7jm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const userCollection = client.db("bicycle-manufacturer").collection("users");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Winged Wheels Bicycle Manufacturer Running");
 });
