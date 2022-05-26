21a000358462dae66a0ff07d1958a498462474a7
shova29 <halima2996@gmail.com>
2022-05-27 05:43:52 +0600

    user get api added

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
       .db("bicycle-manufacturer")
       .collection("purchases");
 
+    //User Get Api
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
