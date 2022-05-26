e7173d4320edec47bc7cec06ef9c396d5f79cd02
shova29 <halima2996@gmail.com>
2022-05-27 05:47:03 +0600

    admin put api added

--- index.js
+++ index.js
@@ -55,6 +55,17 @@ async function run() {
       res.send({ result, token });
     });
 
+    //Admin Put Api
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Part API
     app.get("/part", async (req, res) => {
       const query = {};
