b4a52d42fd04c2bff420caf07c4d058a3afe0d22
shova29 <halima2996@gmail.com>
2022-07-07 22:58:21 +0600

    updated admin get api

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function run() {
     });
 
     //Admin Get Api
-    app.get("/admin/:email", async (req, res) => {
+    app.get("/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === "admin";
